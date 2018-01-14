b48e4d251d609e6f7233732fab635378ebe3f756
Ksenia Morozova <kseniamorozova@kseniamorozova-17TFVH8.dyn.pinadmin.com>
2018-01-14 22:56:19 +0000

    implemented grid

--- designs.js
+++ designs.js
@@ -3,8 +3,50 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+const table = document.getElementById('pixel_canvas');
 
-// Your code goes here!
+$( "#make_grid" ).click(function() {
+	resetGrid();
+  	makeGrid();
+});
 
+function makeGrid(){
+var height = document.getElementById('input_height').value;
+var width = document.getElementById("input_width").value;
+	for(var rowNum=0; rowNum <= height; rowNum++){
+	var row = table.insertRow(rowNum);
+		for(var cellNum=0; cellNum <= width; cellNum++){
+			row.insertCell(cellNum);
+	}
 }
+
+};
+
+function resetGrid(){
+	table.innerHTML = "";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
